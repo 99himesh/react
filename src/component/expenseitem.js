bd8081cc06cd3871a36e5cf136584bd194034b38
himesh <himeshkashyap347@gmail.com>
2022-12-20 10:41:24 -0800

    task7

---
+++ src/component/expenseitem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return  (
+        <div>
+            <div>
+                <h2>Expense item</h2>
+            </div>
+            <div>
+                <p>Food Rs.10</p>
+                <p>Petrol Rs 100</p>
+                <p>Movies Rs 200</p>
+            </div>
+         
+        </div>
+    );
+    
+}
+export default ExpenseItem;
