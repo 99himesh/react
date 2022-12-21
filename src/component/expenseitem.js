09f73a1728360ad9f2714a746c903fa5fa89bc60
himesh <himeshkashyap347@gmail.com>
2022-12-20 23:19:19 -0800

    task 8 2

--- src/component/expenseitem.js
+++ src/component/expenseitem.js
@@ -4,6 +4,7 @@ function ExpenseItem(){
    const expenseDate=new Date(2021,2,28);
    const expenseTitle='car Insourence';
    const expenseAmount=294.76;
+   const LocationOfExpenditure='Lucknow';
 
 
     return  (
@@ -12,7 +13,9 @@ function ExpenseItem(){
             <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
+                <div>{LocationOfExpenditure}</div>
             <div className='expense-item__price'>${expenseAmount} </div>
+           
             </div>
         </div>
     );
