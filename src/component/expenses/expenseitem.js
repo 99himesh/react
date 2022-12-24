1190da3d85d10c66a89405805b63780ec8da96a5
himesh <himeshkashyap347@gmail.com>
2022-12-24 07:00:50 -0800

    task 4(aduance react

--- src/component/expenses/expenseitem.js
+++ src/component/expenses/expenseitem.js
@@ -1,21 +1,30 @@
+import React,{useState} from 'react';
+
 import './Expenseitem.css';
 import ExpenseDate from './ExpenceDate';
 import ExpenseDetails from './expenses';
 import card from '../UI/card'
 const ExpenseItem=(props)=>{
 
-    const DeleteExpense=()=>{
-        console.log('delete ');
-    }
+    // const DeleteExpense=()=>{
+    //     console.log('delete ');
+    // }
+
+  const [ title,setTitle]=   useState(props.title);
+  const [amount,setAmount]=useState(props.amount);
+   const channgeprice=()=>{
+      setAmount('100');
+   }
+
     const clickedhandler=()=>{
-        console.log('clicked!!!');
+        setTitle("updated");       
     }
     return  (
           <card className='expense-item' >
             <ExpenseDate  date={props.date}></ExpenseDate>
-            <ExpenseDetails title = {props.title}  amount = {props.amount}/>
+            <ExpenseDetails title = {title}  amount = {amount}/>
            <button onClick={clickedhandler}>Change title</button>
-           <button onClick={DeleteExpense}>Delete Expense</button>
+           <button onClick={channgeprice}>change Expense</button>
         </card>
     ); 
 }
