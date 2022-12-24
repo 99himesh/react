f8109d44ce7dae21cecb80b5a34f2d3024d3e95e
himesh <himeshkashyap347@gmail.com>
2022-12-24 13:38:14 -0800

    task 6(aduance react)

--- src/component/new expense/ExpenseForm.js
+++ src/component/new expense/ExpenseForm.js
@@ -6,15 +6,42 @@ const ExpenseForm=()=>{
   const [enteredDate,setEnteredDate]=useState('');
   
 
+// const [userInput,setUserInput]=useState({
+//     enteredTitle:'',
+//     enteredAmount:'',
+//     enteredDate:''
+// });
+
      const titleChangeHandler=(event)=>{
      setEnteredTitle(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle:event.target.value,
+        
+    // })
+    // setUserInput((prevState)=>{
+    //      return {...prevState,enteredTitle:event.target.value}
+    // })
+     
      } 
      const AmountChangeHandeler=(event)=>{
         setEnteredAmount(event.target.value);
-        }
+        // }
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:event.target.value,
+            
+        // })
+    }
         const DateChangeHandeler=(event)=>{
             setEnteredDate(event.target.value);
-            }
+            // setUserInput({
+            //     ...userInput,
+            //     enteredDate:event.target.value,
+            
+            // });
+        }
+        
     return (
     <form>
         < div className="new-expense__controls">
