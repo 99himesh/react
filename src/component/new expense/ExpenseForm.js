9395200b22be79195a31ad4725263f9fbe047f9f
himesh <himeshkashyap347@gmail.com>
2022-12-24 14:12:12 -0800

    task 7(aduanse react)

--- src/component/new expense/ExpenseForm.js
+++ src/component/new expense/ExpenseForm.js
@@ -4,14 +4,11 @@ const ExpenseForm=()=>{
   const [enteredTitle,setEnteredTitle]=  useState('');
   const [enteredAmount,setEnteredAmount]=useState('');
   const [enteredDate,setEnteredDate]=useState('');
-  
-
 // const [userInput,setUserInput]=useState({
 //     enteredTitle:'',
 //     enteredAmount:'',
 //     enteredDate:''
 // });
-
      const titleChangeHandler=(event)=>{
      setEnteredTitle(event.target.value);
     // setUserInput({
@@ -40,10 +37,19 @@ const ExpenseForm=()=>{
             //     enteredDate:event.target.value,
             
             // });
-        }
-        
+        };
+const submitHandler=(event)=>{
+    event.preventDefault();
+    const expenseData={
+       title:enteredTitle,
+       amount:enteredAmount,
+       date:new Date(enteredDate)
+    };
+    console.log(expenseData);
+}
+
     return (
-    <form>
+    <form onSubmit={submitHandler} >
         < div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
